2d213d57e38d1570e587a1c24728104da324a5a1
Prem2310 <premrpatel.cse21@aii.ac.in>
2024-07-14 12:55:13 +0530

    clerk

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/LandingPage";
 import { BookSearchPage } from "./pages/BookSearchPage";
 import { AdminSearchPage } from "./pages/AdminSearchPage";
 
-
+import Signin from "./pages/SignIn";
+import Signup from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/search-books" element={<BookSearchPage/>} />
-          <Route path="/admin-books" element={<AdminSearchPage/>} />
+          <Route path="/signin" element={<Signin />} />
+
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/search-books" element={<BookSearchPage />} />
+          <Route path="/admin-books" element={<AdminSearchPage />} />
         </Routes>
       </BrowserRouter>
     </>
