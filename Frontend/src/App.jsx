aded96b520e082dc9466a34765dff0d2f5dd2538
Khushi Shukla <khushishukla2323@gmail.com>
2024-07-14 10:03:59 +0530

    Navbar and sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,9 +6,10 @@ import { Home } from "./pages/LandingPage";
 function App() {
   return (
     <>
+      
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
